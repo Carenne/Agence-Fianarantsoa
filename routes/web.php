afe581b17659559b8fcdea7971b387c3438264f0
Carenne <karenrasoanirina@gmail.com>
2025-07-31 18:55:50 +0200

    roout

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+  //  return view('/admin/proprietes/index');
+    return view('/admin/proprietes/index', [
+        'proprietes' => \App\Models\propriete::all()
+    ]);
+   // return view('welcome');
+
 });
 
 Route::prefix('admin')->name('admin.')->group(function(){
